07a4213ab688aa64d6cef9b2c84ea1edb28a8080
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2023-11-12 15:30:23 +0000

    feat(main):farmer class test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import net.salesiano.agricultura.granjeros.Granjeros;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Granjeros paco = new Granjeros("Paco", 10);
+        paco.start();
     }
 }
