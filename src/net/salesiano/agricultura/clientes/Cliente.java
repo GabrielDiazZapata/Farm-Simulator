0c06c8659fc8ba4b2e3e184cb05ee2b107529607
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2023-11-12 23:01:13 +0000

    wip(feature/cliente: created branch and class Cliente)

---
+++ src/net/salesiano/agricultura/clientes/Cliente.java
@@ -0,0 +1,17 @@
+package net.salesiano.agricultura.clientes;
+
+public class Cliente extends Thread {
+    private String nombre;
+    private int cantidadVerduras;
+
+    public Cliente(String nombre, int cantidadVerduras){
+        this.nombre = nombre;
+        this.cantidadVerduras = cantidadVerduras;
+    }
+
+    public void consumir() throws InterruptedException{
+        
+
+
+    }
+}
