b47fb52fae6293174bb0a7afe6eb3004477390fb
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2023-11-15 16:04:58 +0000

    feat(experimento-tres): experiment three completed and readme changed

--- src/App.java
+++ src/App.java
@@ -5,39 +5,42 @@ import net.salesiano.agricultura.restaurante.Restaurante;
 public class App {
     public static void main(String[] args)  {
        // Aquí se crean unas instancias de granjeros, clientes y restaurante
-       Granjero gonzalo = new Granjero("Gonzalo", 10);
-       Granjero pepe = new Granjero("Pepe", 10);
+       Granjero gonzalo = new Granjero("Gonzalo", 5);
+      //  Granjero pepe = new Granjero("Pepe", 10);
 
        Cliente laPinta = new Cliente("La Pinta", 5);
-       Cliente laNiña = new Cliente("La Niña", 10);
-       Cliente laSantaMaria = new Cliente("Y la Santa Maria", 5);
+      //  Cliente laNiña = new Cliente("La Niña", 10);
+      //  Cliente laSantaMaria = new Cliente("Y la Santa Maria", 5);
 
        Restaurante restaurante = new Restaurante(15);
 
        // Se le asigna el restaurante a los granjeros y clientes
        gonzalo.setRestaurante(restaurante);
-       pepe.setRestaurante(restaurante);
+      //  pepe.setRestaurante(restaurante);
 
        laPinta.setRestaurante(restaurante);
-       laNiña.setRestaurante(restaurante);
-       laSantaMaria.setRestaurante(restaurante);
+      //  laNiña.setRestaurante(restaurante);
+      //  laSantaMaria.setRestaurante(restaurante);
 
         // iniciamos los hilos
        gonzalo.start();
-       pepe.start();
+      //  pepe.start();
        laPinta.start();
-       laNiña.start();
-       laSantaMaria.start();
+      //  laNiña.start();
+      //  laSantaMaria.start();
+
+      gonzalo.setPriority(Thread.MAX_PRIORITY);
+      laPinta.setPriority(Thread.MIN_PRIORITY);
 
 
 
        try {
          // Lo que va hacer es esperar a que todos los hilos terminen
          gonzalo.join();
-         pepe.join();
+        //  pepe.join();
          laPinta.join();
-         laNiña.join();
-         laSantaMaria.join();
+        //  laNiña.join();
+        //  laSantaMaria.join();
        } catch (InterruptedException e) {
         e.printStackTrace();
         }
